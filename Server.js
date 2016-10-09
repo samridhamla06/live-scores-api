6408d9d75029a3abe898ee5b06ae7fd4af4dbc43
samridhamla06 <samridhamla06@Samridhs-MacBook-Pro.local>
2016-10-10 02:38:32 +0530

    deleting irrelevant files

--- Server.js
+++
@@ -1,6 +0,0 @@
-var app = require(__dirname + '/app.js');
-var server = require('http').createServer(app);
-
-server.listen(8000,function(err){
-    console.log('Server is running at 8000 port')
-});
